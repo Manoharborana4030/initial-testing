cdf0e50eacac4e0de4ad3615f1ba26ea90554293
arjun <arjundarji9995@outlook.com>
2022-03-10 12:33:44 +0530

    update arjun

--- arjun.py
+++ arjun.py
@@ -6,25 +6,7 @@ print("All changes in this file will only be done by Arjun")
 print("All changes in this file will only be done by Arjun")
 print("All changes in this file will only be done by Arjun")
 print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
-print("All changes in this file will only be done by Arjun")
+print("All aaaaaaaaaaaaa
+	aaaaaaaaaaaaaaaaaaa
+	aaaaaaaaaaaaaaaaaaaaaa
+	kjlsbdbhnkjsabnjid
